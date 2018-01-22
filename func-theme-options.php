eec736c804f8b439662d146765ba3b5d0a60137f
Rodrigo Mejía <contacto@habitatweb.com.mx>
2018-01-22 16:32:41 -0600

    Theme renamed, readme updated, code review

---
+++ func-theme-options.php
@@ -0,0 +1,178 @@
+<?php
+// START THEME OPTIONS
+// custom theme options for user in admin area - Appearance > Theme Options
+function pu_theme_menu()
+{
+  add_theme_page( 'Theme Option', 'Theme Options', 'manage_options', 'pu_theme_options.php', 'pu_theme_page');  
+}
+add_action('admin_menu', 'pu_theme_menu');
+
+function pu_theme_page()
+{
+?>
+    <div class="section panel">
+      <h1>Custom Theme Options</h1>
+      <form method="post" enctype="multipart/form-data" action="options.php">
+      <hr>
+        <?php 
+
+          settings_fields('pu_theme_options'); 
+        
+          do_settings_sections('pu_theme_options.php');
+          echo '<hr>';
+        ?>
+            <p class="submit">  
+                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />  
+            </p>
+      </form>
+    </div>
+    <?php
+}
+
+add_action( 'admin_init', 'pu_register_settings' );
+
+/**
+ * Function to register the settings
+ */
+function pu_register_settings()
+{
+    // Register the settings with Validation callback
+    register_setting( 'pu_theme_options', 'pu_theme_options' );
+
+    // Add settings section
+    add_settings_section( 'pu_text_section', 'Social Links', 'pu_display_section', 'pu_theme_options.php' );
+
+    // Create textbox field
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'twitter_link',
+      'name'      => 'twitter_link',
+      'desc'      => 'Twitter Link - Example: http://twitter.com/username',
+      'std'       => '',
+      'label_for' => 'twitter_link',
+      'class'     => 'css_class'
+    );
+
+    // Add twitter field
+    add_settings_field( 'twitter_link', 'Twitter', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );   
+
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'facebook_link',
+      'name'      => 'facebook_link',
+      'desc'      => 'Facebook Link - Example: http://facebook.com/username',
+      'std'       => '',
+      'label_for' => 'facebook_link',
+      'class'     => 'css_class'
+    );
+
+    // Add facebook field
+    add_settings_field( 'facebook_link', 'Facebook', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );
+
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'youtube_link',
+      'name'      => 'youtube_link',
+      'desc'      => 'Youtube Link - Example: https://www.youtube.com/channel/channel_id',
+      'std'       => '',
+      'label_for' => 'youtube_link',
+      'class'     => 'css_class'
+    );
+
+    // Add youtube field
+    add_settings_field( 'youtube_ink', 'Youtube', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );
+
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'instagram_link',
+      'name'      => 'instagram_link',
+      'desc'      => 'Instagram Link - Example: http://instagram.com/username',
+      'std'       => '',
+      'label_for' => 'instagram_link',
+      'class'     => 'css_class'
+    );
+
+    // Add Instagram field
+    add_settings_field( 'instagram_link', 'Instagram', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );
+
+}
+
+// helper for inserting social links
+function social_links($args)
+{
+  $social_links = get_option( 'pu_theme_options' );
+  if($social_links[$args.'_link']) {
+    echo('
+      <a href="'.$social_links[$args.'_link'].'" target="_blank">
+      <img src="' . get_bloginfo('template_url') . '/icons/'.$args.'.svg" >
+      </a>');
+  }
+}
+
+// allow wordpress post editor functions to be used in theme options
+function pu_display_setting($args)
+{
+    extract( $args );
+
+    $option_name = 'pu_theme_options';
+
+    $options = get_option( $option_name );
+
+    switch ( $type ) {  
+          case 'text':  
+              $options[$id] = stripslashes($options[$id]);  
+              $options[$id] = esc_attr( $options[$id]);  
+              echo "<input class='regular-text$class' type='text' id='$id' name='" . $option_name . "[$id]' value='$options[$id]' />";  
+              echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
+          break;
+          case 'textarea':  
+              $options[$id] = stripslashes($options[$id]);  
+              //$options[$id] = esc_attr( $options[$id]);
+              $options[$id] = esc_html( $options[$id]); 
+
+              printf(
+                wp_editor($options[$id], $id, 
+                  array('textarea_name' => $option_name . "[$id]",
+                    'style' => 'width: 200px'
+                    )) 
+        );
+              // echo "<textarea id='$id' name='" . $option_name . "[$id]' rows='10' cols='50'>".$options[$id]."</textarea>";  
+              // echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";  
+          break; 
+    }
+}
+
+function pu_validate_settings($input)
+{
+  foreach($input as $k => $v)
+  {
+    $newinput[$k] = trim($v);
+    
+    // Check the input is a letter or a number
+    if(!preg_match('/^[A-Z0-9 _]*$/i', $v)) {
+      $newinput[$k] = '';
+    }
+  }
+
+  return $newinput;
+}
+
+// Add custom styles to theme options area
+add_action('admin_head', 'custom_style');
+
+function custom_style() {
+  echo '<style>
+    .appearance_page_pu_theme_options .wp-editor-wrap {
+      width: 75%;
+    }
+    .regular-textcss_class {
+      width: 50%;
+    }
+    .appearance_page_pu_theme_options h3 {
+      font-size: 2em;
+      padding-top: 40px;
+    }
+  </style>';
+}
+
+// END THEME OPTIONS
--- readme.md
+++ readme.md
@@ -1,26 +1,21 @@
-#blank-bootstrap-gulp-sass-wp-theme
-Based on
-[Bootstrap-3-blank-wordpress-theme](https://github.com/sebastienb/Bootstrap-3-blank-wordpress-theme)
+# HW Blank Bootstrap Wordpress Theme
 
-I created this theme from the Bootstrap 3 Starter Wordpress theme by 3sparks as a starting point for my theme development. It adds **Gulp** and **Sass** support and fixes some issues found in the source theme. **It's aim is to be as simple and small as possible.** Contributions in the direction of having a cleaner theme would be greately welcome!
+I created this theme from the Bootstrap 3 Starter Wordpress [theme by 3sparks](https://github.com/sebastienb/Bootstrap-3-blank-wordpress-theme) as a starting point for my theme development. It adds **Gulp** and **Sass** support and fixes some issues I found in the source theme. **It's aim is to be as simple and small as possible.** Contributions in the direction of having a cleaner theme would be greately welcome!
 
 
-###Setup
+### Setup
 * After installing WordPress, clone this repository within the themes directory (and rename it as you wish).
 * [Download the Bootrstap Sass version](http://getbootstrap.com/getting-started/#download) and put it in your theme src/ directory and rename the directory as "bootstrap-sass" (without version). Alternatively, you can clone it from the [Bootstrap Sass Repository](https://github.com/twbs/bootstrap-sass).
 * Run `npm install` and then run `gulp` to start watching your files. Use the [livereload browser extension](http://livereload.com/extensions/) to have your browser automatically refreshed.
 
-###To customize Bootstrap
+### To customize Bootstrap
 * To use the bootstrap variables you can edit the original assets/stylesheets/bootstrap/_variables.scss or use any of them directly in your css/bootstrap.scss file, just before the `@import`. This way you can keep your variables if you update your bootstrap version.
 * To optimize your final Bootstrap css, comment in assets/stylesheets/_bootstrap.css all the components you're not using.
 
-
-###Function file includes
-* Menu Support
+### Function file includes
+* Menu Support (main manu and footer menu)
 * 1 registered sidebar
 * Theme options for storing Social Links (and helper function to show them)
-* Post thumbnails (not tested yet)
-* Gravity forms submit button bootstrap btn class (not tested yet)
 
-###Known issues
+### Known issues
 * Menu doesn't work correctly with 3 or more levels of depth
