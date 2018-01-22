bc6408906ce3d5665dbd33160dc0365a0005d742
Rodrigo Mejía <contacto@habitatweb.com.mx>
2018-01-21 20:27:27 -0600

    Ajustes footer

--- footer.php
+++ footer.php
@@ -1,27 +1,29 @@
+</div> <!-- close main container -->
 <footer>
-  <nav>
-    <?php wp_nav_menu( array(
-      'theme_location'  => 'footer',
-      // Evita que se agregen automáticamente todas las pags de wp_page_menu cuando no hay menú asignado
-      'fallback_cb' => function(){},
-    ) ); ?>
-  </nav>
-  <?php bloginfo( 'name' ); ?>
-  <div class="social">
-  <?php
-  // social links, using the svgs inside the icons folder
-  // sample icons are from https://dribbble.com/shots/2089345-Nucleo-Free-Social-Icons
-  social_links('facebook');
-  social_links('twitter');
-  social_links('youtube');
-  social_links('instagram');
-  ?>
+  <div class="container">
+    <nav>
+      <?php wp_nav_menu( array(
+        'theme_location'  => 'footer',
+        // Evita que se agregen automáticamente todas las pags de wp_page_menu cuando no hay menú asignado
+        'fallback_cb' => function(){},
+      ) ); ?>
+    </nav>
+    <?php bloginfo( 'name' ); ?>
+    <div class="social">
+    <?php
+    // social links, using the svgs inside the icons folder
+    // sample icons are from https://dribbble.com/shots/2089345-Nucleo-Free-Social-Icons
+    social_links('facebook');
+    social_links('twitter');
+    social_links('youtube');
+    social_links('instagram');
+    ?>
 
-  </div>
+    </div>
 
 <?php wp_footer(); ?>
+  </div> <!-- Close footer container -->
 </footer>
 
-</div> <!-- close main container -->
 </body>
 </html>
