81cea76c0b76a5e97c6d80cb3ffea60f5eba1bdc
Rodrigo Mejía <contacto@habitatweb.com.mx>
2018-02-06 09:27:52 -0600

    Footer improvements

--- footer.php
+++ footer.php
@@ -1,27 +1,32 @@
 <footer>
   <div class="container">
-    <nav>
-      <?php wp_nav_menu( array(
-        'theme_location'  => 'footer',
-        // It avoids that all the pages of wp_page_menu are automatically added when there is no assigned menu
-        'fallback_cb' => function(){},
-      ) ); ?>
-    </nav>
-    <?php bloginfo( 'name' ); ?>
-    <div class="social">
-    <?php
-    // social links, using the svgs inside the icons folder
-    // sample icons are from https://dribbble.com/shots/2089345-Nucleo-Free-Social-Icons
-    social_links('facebook');
-    social_links('twitter');
-    social_links('youtube');
-    social_links('instagram');
-    ?>
+    <div class="row">
+        <div class="col-md-8">
+            <nav>
+              <?php wp_nav_menu( array(
+                'theme_location'  => 'footer',
+                // It avoids that all the pages of wp_page_menu are automatically added when there is no assigned menu
+                'fallback_cb' => function(){},
+              ) ); ?>
+            </nav>
+            <?php bloginfo( 'name' ); echo '. '; bloginfo( 'description' ); ?>
+        </div>
+        <div class="col-md-4">
+            <div class="social">
+            <?php
+            // social links, using the svgs inside the icons folder
+            // sample icons are from https://dribbble.com/shots/2089345-Nucleo-Free-Social-Icons
+            social_links('facebook');
+            social_links('twitter');
+            social_links('youtube');
+            social_links('instagram');
+            ?>
 
+            </div>
+        </div>
     </div>
-
-<?php wp_footer(); ?>
   </div> <!-- Close footer container -->
+<?php wp_footer(); ?>
 </footer>
 
 </body>
