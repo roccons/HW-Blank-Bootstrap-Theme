36237de759d2be941473138bf1780f40122c183f
rosendito <rosendojose0617@gmail.com>
2020-06-26 15:02:41 -0400

    Change IMG_PATH

--- includes/theme-options.php
+++ includes/theme-options.php
@@ -3,7 +3,7 @@
 // custom theme options for user in admin area - Appearance > Theme Options
 function pu_theme_menu()
 {
-  add_theme_page( 'Theme Option', 'Theme Options', 'manage_options', 'pu_theme_options.php', 'pu_theme_page');  
+  add_theme_page( 'Theme Option', 'Theme Options', 'manage_options', 'pu_theme_options.php', 'pu_theme_page');
 }
 add_action('admin_menu', 'pu_theme_menu');
 
@@ -14,15 +14,15 @@ function pu_theme_page()
       <h1>Custom Theme Options</h1>
       <form method="post" enctype="multipart/form-data" action="options.php">
       <hr>
-        <?php 
+        <?php
 
-          settings_fields('pu_theme_options'); 
-        
+          settings_fields('pu_theme_options');
+
           do_settings_sections('pu_theme_options.php');
           echo '<hr>';
         ?>
-            <p class="submit">  
-                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />  
+            <p class="submit">
+                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
             </p>
       </form>
     </div>
@@ -54,7 +54,7 @@ function pu_register_settings()
     );
 
     // Add twitter field
-    add_settings_field( 'twitter_link', 'Twitter', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );   
+    add_settings_field( 'twitter_link', 'Twitter', 'pu_display_setting', 'pu_theme_options.php', 'pu_text_section', $field_args );
 
     $field_args = array(
       'type'      => 'text',
@@ -104,7 +104,7 @@ function social_links($args)
   if($social_links[$args.'_link']) {
     echo('
       <a href="'.$social_links[$args.'_link'].'" target="_blank">
-      <img src="' . get_bloginfo('template_url') . '/img/icons/'.$args.'.svg" >
+      <img src="' . get_bloginfo('template_url') . 'assets/img/icons/'.$args.'.svg" >
       </a>');
   }
 }
@@ -118,27 +118,27 @@ function pu_display_setting($args)
 
     $options = get_option( $option_name );
 
-    switch ( $type ) {  
-          case 'text':  
-              $options[$id] = stripslashes($options[$id]);  
-              $options[$id] = esc_attr( $options[$id]);  
-              echo "<input class='regular-text$class' type='text' id='$id' name='" . $option_name . "[$id]' value='$options[$id]' />";  
+    switch ( $type ) {
+          case 'text':
+              $options[$id] = stripslashes($options[$id]);
+              $options[$id] = esc_attr( $options[$id]);
+              echo "<input class='regular-text$class' type='text' id='$id' name='" . $option_name . "[$id]' value='$options[$id]' />";
               echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
           break;
-          case 'textarea':  
-              $options[$id] = stripslashes($options[$id]);  
+          case 'textarea':
+              $options[$id] = stripslashes($options[$id]);
               //$options[$id] = esc_attr( $options[$id]);
-              $options[$id] = esc_html( $options[$id]); 
+              $options[$id] = esc_html( $options[$id]);
 
               printf(
-                wp_editor($options[$id], $id, 
+                wp_editor($options[$id], $id,
                   array('textarea_name' => $option_name . "[$id]",
                     'style' => 'width: 200px'
-                    )) 
+                    ))
         );
-              // echo "<textarea id='$id' name='" . $option_name . "[$id]' rows='10' cols='50'>".$options[$id]."</textarea>";  
-              // echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";  
-          break; 
+              // echo "<textarea id='$id' name='" . $option_name . "[$id]' rows='10' cols='50'>".$options[$id]."</textarea>";
+              // echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
+          break;
     }
 }
 
@@ -147,7 +147,7 @@ function pu_validate_settings($input)
   foreach($input as $k => $v)
   {
     $newinput[$k] = trim($v);
-    
+
     // Check the input is a letter or a number
     if(!preg_match('/^[A-Z0-9 _]*$/i', $v)) {
       $newinput[$k] = '';
